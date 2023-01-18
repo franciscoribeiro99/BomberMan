a5faace6d141d95ce039f4a9673f33e4a9ec9a10
Francisco RIbeiro <74601529+franciscoribeiro99@users.noreply.github.com>
2023-01-18 14:20:07 +0100

    Replay Function

--- src/GamePlay.scala
+++ src/GamePlay.scala
@@ -1,9 +1,10 @@
 object GamePlay extends App {
   val grid1 = new Grid()
-  do{
+
+  while (true) {
+    // Runs the game once
     grid1.startplay()
     println(grid1.displayGrid())
-  }while(grid1.replay==true && grid1.gameover==true)
-
+  }
 
 }
--- src/Grid.scala
+++ src/Grid.scala
@@ -8,71 +8,125 @@ import java.awt.event.{KeyAdapter, KeyEvent}
 
 class Grid {
   //implemente la zone de jeu 12x7
+
+  /**
+   * Things that have to be re-init between games
+   */
+
+
+  //l'énemi doit toujours bouger!
+  var memE1x = 2
+  var memE1y = 15
+  var memE2x = 5
+  var memE2y = 15
+  var memE3x = 9
+  var memE3y = 15
+  var timetoexplosion: Int = 0
+  var grille: Array[Array[String]] = Array.ofDim(gridX, gridY)
+
+  // What are we currently doing and displaying (mui)
+  var game_state: Int = STATE_PLAYING
+
+  /**
+   * Things that remain between different games
+   */
   val gridX = 13
   val gridY = 19
-  var timetoexplosion: Int = 0
-  var gameover: Boolean = false
-  val grille: Array[Array[String]] = Array.ofDim(gridX, gridY)
-  val display = new FunGraphics(950, 750, "BomberMan")
-  var replay : Boolean = false
 
+  val STATE_PLAYING: Int = 0
+  val STATE_SHOWING_GAME_OVER: Int = 1
+
+  val display = new FunGraphics(950, 750, "BomberMan")
 
   //images
   val grass = new GraphicsBitmap("/img/grass.jpg")
   val woodBox = new GraphicsBitmap("/img/WoodBox.png")
-  val StoneWall = new GraphicsBitmap("/img/StoneWall.png")
-  val BomberMan = new GraphicsBitmap("/img/Bomberman.png")
-  val Enemy = new GraphicsBitmap("/img/ennemy.png")
-  val Logo = new GraphicsBitmap("/img/logo.png")
+  val stoneWall = new GraphicsBitmap("/img/StoneWall.png")
+  val bomberMan = new GraphicsBitmap("/img/Bomberman.png")
+  val enemyImg = new GraphicsBitmap("/img/ennemy.png")
+  val gameLogo = new GraphicsBitmap("/img/logo.png")
   val bomb = new GraphicsBitmap("/img/bomb.png")
   val explosionimg = new GraphicsBitmap("/img/explosion.png")
   val gameoverimg = new GraphicsBitmap("/img/gameover.png")
 
+  construct()
 
-  //liaison avec les touches
-  display.setKeyManager(new KeyAdapter() { // Will be called when a key has been pressed
-    override def keyPressed(e: KeyEvent): Unit = {
-      if (e.getKeyCode == KeyEvent.VK_UP) move('w')
-      if (e.getKeyCode == KeyEvent.VK_DOWN) move('s')
-      if (e.getKeyCode == KeyEvent.VK_LEFT) move('a')
-      if (e.getKeyCode == KeyEvent.VK_RIGHT) move('d')
-      if (e.getKeyCode == KeyEvent.VK_SPACE) if (timetoexplosion == 0) move('x')
-      if (e.getKeyCode == KeyEvent.VK_ENTER) if (gameover == true) {
-       replay = true
+  def construct(): Unit = {
+    //liaison avec les touches
+    display.setKeyManager(new KeyAdapter() { // Will be called when a key has been pressed
+      override def keyPressed(e: KeyEvent): Unit = {
+        if (game_state == STATE_PLAYING) {
+          if (e.getKeyCode == KeyEvent.VK_UP) move('w')
+          if (e.getKeyCode == KeyEvent.VK_DOWN) move('s')
+          if (e.getKeyCode == KeyEvent.VK_LEFT) move('a')
+          if (e.getKeyCode == KeyEvent.VK_RIGHT) move('d')
+          if (e.getKeyCode == KeyEvent.VK_SPACE) if (timetoexplosion == 0) move('x')
+        }
+        // FIXME : comment this properly
+        if (game_state == STATE_SHOWING_GAME_OVER) {
+          if (e.getKeyCode == KeyEvent.VK_ENTER) {
+            println("Enter pressed")
+            game_state = STATE_PLAYING
+          }
+          // FIXME : comment this properly
+          if (e.getKeyCode == KeyEvent.VK_ESCAPE) {
+            System.exit(-1)
+          }
+        }
       }
-      if (e.getKeyCode == KeyEvent.VK_ESCAPE) if (gameover == true) System.exit(-1)
-    }
-  })
-
+    })
+  }
 
   //InitPlaymode
   def startplay(): Unit = {
-    gameover = false
+    game_state = STATE_PLAYING
+    timetoexplosion = 0
+    grille = Array.ofDim(gridX, gridY)
+
+    //FIXME: explain all this
+    memE1x = 2
+    memE1y = 15
+    memE2x = 5
+    memE2y = 15
+    memE3x = 9
+    memE3y = 15
+
+
     initGrid()
     setWall()
     addPlayer()
     createEnemy()
     setRemovalWall(10)
     println(displayGrid())
-    while (true) {
+
+    // FIXME : comment this properly
+    while (game_state == STATE_PLAYING) {
       Thread.sleep(300)
       updateGraphics()
       moveenemy()
     }
+
     updateGraphics()
+
+    // FIXME : comment this properly
+    while (game_state == STATE_SHOWING_GAME_OVER) {
+      Thread.sleep(300)
+      println("In state STATE_SHOWING_GAME_OVER")
+      //      updateGraphics()
+    }
 
   }
 
-//updateGraphics on FunGraphics
+  //updateGraphics on FunGraphics
   def updateGraphics(): Unit = {
     val i1 = 50
     var NotfoundP = false
     if (timetoexplosion >= 1)
       timetoexplosion += 1
-    if (timetoexplosion == 20) {
+    if (timetoexplosion == 8) {
       explosion()
     }
-    if (timetoexplosion == 24) {
+    if (timetoexplosion == 10) {
       removeDamage()
       timetoexplosion = 0
     }
@@ -86,10 +140,10 @@ class Grid {
           if (grille(i)(j) == "2")
             NotfoundP = true
           grille(i)(j) match {
-            case "0" => display.drawPicture(j * i1 + 25, 125 + i * i1, StoneWall)
-            case "2" => display.drawPicture(j * i1 + 25, 125 + i * i1, BomberMan)
+            case "0" => display.drawPicture(j * i1 + 25, 125 + i * i1, stoneWall)
+            case "2" => display.drawPicture(j * i1 + 25, 125 + i * i1, bomberMan)
             case " " =>
-            case "E" => display.drawPicture(j * i1 + 25, 125 + i * i1, Enemy)
+            case "E" => display.drawPicture(j * i1 + 25, 125 + i * i1, enemyImg)
             case "W" => display.drawPicture(j * i1 + 25, 125 + i * i1, woodBox)
             case "X" => display.drawPicture(j * i1 + 25, 125 + i * i1, woodBox)
             case "B" => display.drawPicture(j * i1 + 25, 125 + i * i1, bomb)
@@ -100,11 +154,14 @@ class Grid {
 
       }
     }
-    display.drawPicture(display.width / 2, 75, Logo)
-    if (NotfoundP == false)
-      gameover = true
+    display.drawPicture(display.width / 2, 75, gameLogo)
 
-    if (gameover == true)
+    // FIXME : COMMENT PLEASE
+    // TODO: should be done
+    if (!NotfoundP && game_state == STATE_PLAYING)
+      game_state = STATE_SHOWING_GAME_OVER
+
+    if (game_state == STATE_SHOWING_GAME_OVER)
       display.drawPicture(display.width / 2, display.height / 2, gameoverimg)
   }
 
@@ -156,7 +213,7 @@ class Grid {
 
   }
 
-//initialise le jouer
+  //initialise le jouer
   def addPlayer(): Unit = {
     grille(1)(1) = "2"
   }
@@ -174,7 +231,7 @@ class Grid {
   var membombx = 0
   var membomby = 0
 
-//fonction pour bouger le player
+  //fonction pour bouger le player
   def move(lettre: Char): Unit = {
     val read: Char = lettre
     var r = 0
@@ -281,14 +338,6 @@ class Grid {
       grille(membombx)(membomby - 1) = " "
   }
 
-  //l'énemi doit toujours bouger!
-  var memE1x = 2
-  var memE1y = 15
-  var memE2x = 5
-  var memE2y = 15
-  var memE3x = 9
-  var memE3y = 15
-
 
   //move randomly enemy
   def moveenemy(): Unit = {
@@ -301,27 +350,27 @@ class Grid {
           memE1x = memE1x - 1
         }
         else if (grille(memE1x - 1)(memE1y) == "2")
-          gameover = true
+          game_state = STATE_SHOWING_GAME_OVER
         case 1 => if (grille(memE1x + 1)(memE1y) == " ") {
           grille(memE1x + 1)(memE1y) = "E"
           grille(memE1x)(memE1y) = " "
           memE1x = memE1x + 1
         }
         else if (grille(memE1x + 1)(memE1y) == "2")
-          gameover = true
+          game_state = STATE_SHOWING_GAME_OVER
         case 2 => if (grille(memE1x)(memE1y - 1) == " ") {
           grille(memE1x)(memE1y - 1) = "E"
           grille(memE1x)(memE1y) = " "
           memE1y = memE1y - 1
         }
         else if (grille(memE1x)(memE1y - 1) == "2")
-          gameover = true
+          game_state = STATE_SHOWING_GAME_OVER
         case 3 => if (grille(memE1x)(memE1y + 1) == " ") {
           grille(memE1x)(memE1y + 1) = "E"
           grille(memE1x)(memE1y) = " "
           memE1y = memE1y + 1
         } else if (grille(memE1x)(memE1y + 1) == "2")
-          gameover = true
+          game_state = STATE_SHOWING_GAME_OVER
         case _ =>
 
       }
@@ -334,27 +383,27 @@ class Grid {
           memE2x = memE2x - 1
         }
         else if (grille(memE2x - 1)(memE2y) == "2")
-          gameover = true
+          game_state = STATE_SHOWING_GAME_OVER
         case 1 => if (grille(memE2x + 1)(memE2y) == " ") {
           grille(memE2x + 1)(memE2y) = "E"
           grille(memE2x)(memE2y) = " "
           memE2x = memE2x + 1
         }
         else if (grille(memE2x + 1)(memE2y) == "2")
-          gameover = true
+          game_state = STATE_SHOWING_GAME_OVER
         case 2 => if (grille(memE2x)(memE2y - 1) == " ") {
           grille(memE2x)(memE2y - 1) = "E"
           grille(memE2x)(memE2y) = " "
           memE2y = memE2y - 1
         }
         else if (grille(memE2x)(memE2y - 1) == "2")
-          gameover = true
+          game_state = STATE_SHOWING_GAME_OVER
         case 3 => if (grille(memE2x)(memE2y + 1) == " ") {
           grille(memE2x)(memE2y + 1) = "E"
           grille(memE2x)(memE2y) = " "
           memE2y = memE2y + 1
         } else if (grille(memE2x)(memE2y + 1) == "2")
-          gameover = true
+          game_state = STATE_SHOWING_GAME_OVER
         case _ =>
 
       }
@@ -367,27 +416,27 @@ class Grid {
           memE3x = memE3x - 1
         }
         else if (grille(memE3x - 1)(memE3y) == "2")
-          gameover = true
+          game_state = STATE_SHOWING_GAME_OVER
         case 1 => if (grille(memE3x + 1)(memE3y) == " ") {
           grille(memE3x + 1)(memE3y) = "E"
           grille(memE3x)(memE3y) = " "
           memE3x = memE3x + 1
         }
         else if (grille(memE3x + 1)(memE3y) == "2")
-          gameover = true
+          game_state = STATE_SHOWING_GAME_OVER
         case 2 => if (grille(memE3x)(memE3y - 1) == " ") {
           grille(memE3x)(memE3y - 1) = "E"
           grille(memE3x)(memE3y) = " "
           memE3y = memE3y - 1
         }
         else if (grille(memE3x)(memE3y - 1) == "2")
-          gameover = true
+          game_state = STATE_SHOWING_GAME_OVER
         case 3 => if (grille(memE3x)(memE3y + 1) == " ") {
           grille(memE3x)(memE3y + 1) = "E"
           grille(memE3x)(memE3y) = " "
           memE3y = memE3y + 1
         } else if (grille(memE3x)(memE3y + 1) == "2")
-          gameover = true
+          game_state = STATE_SHOWING_GAME_OVER
         case _ =>
 
       }
